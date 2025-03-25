20f8d403ff4085ce2f4890790fea07c6f2036328
Sky Brewer <sky.brewer@ess.eu>
2025-03-25 15:17:07 +0100

    Add resume command

--- src/archiver_mgmt_operations/commands/pause_resume.py
+++ src/archiver_mgmt_operations/commands/pause_resume.py
@@ -53,9 +53,45 @@ def pause(archiver_fqdn: str, pvs: list[str]) -> None:
     try:
         pause_results = [archiver.pause_pv(pv) for pv in pvs]
     except HTTPError as e:
-        LOG.error("Error archiving PVs: %s", str(e))  # noqa: TRY400
-        LOG.debug("Error archiving PVs.", exc_info=True)
+        LOG.error("Error pausing PVs: %s", str(e))  # noqa: TRY400
+        LOG.debug("Error pausing PVs.", exc_info=True)
         raise RequestHTTPError(e) from e
 
     # Validate output
     validate_operation_results(pvs, [cast("OperationResult", result) for result in pause_results], "paused")
+
+
+def resume(archiver_fqdn: str, pvs: list[str]) -> None:
+    """The resume command to resume PVs.
+
+    Args:
+        archiver_fqdn (str): The fully qualified domain name of the archiver.
+        pvs (list[str]): The PVs to resume.
+
+    Raises:
+        RequestHTTPError: If there is an error resuming the PVs.
+    """
+    # Validate input
+    archiver_info = ArchiverMgmtInfo(archiver_fqdn)
+    validate_pvs_status(
+        archiver_info,
+        pvs,
+        [
+            ArchivingStatus.Paused,
+        ],
+    )
+
+    # Action
+    LOG.info("Resuming PVs %s", pvs)
+    archiver = ArchiverMgmtOperations(archiver_fqdn)
+    LOG.info("Using archiver %s", archiver.info)
+
+    try:
+        resume_results = [archiver.resume_pv(pv) for pv in pvs]
+    except HTTPError as e:
+        LOG.error("Error resuming PVs: %s", str(e))  # noqa: TRY400
+        LOG.debug("Error resuming PVs.", exc_info=True)
+        raise RequestHTTPError(e) from e
+
+    # Validate output
+    validate_operation_results(pvs, [cast("OperationResult", result) for result in resume_results], "resumed")
