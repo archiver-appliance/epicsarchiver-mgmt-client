7c7ba830fbd651aa4ac612408653a2cafd54536a
Sky Brewer <sky.brewer@ess.eu>
2025-08-26 16:14:16 +0200

    Faster get statuses

--- src/epicsarchiver_mgmt/commands/statuses.py
+++ src/epicsarchiver_mgmt/commands/statuses.py
@@ -13,6 +13,29 @@ if TYPE_CHECKING:
 LOG: logging.Logger = logging.getLogger(__name__)
 
 
+MAX_PVS = 1000
+
+
+def get_statuses_from_archiver(archiver_info: ArchiverMgmtInfo, pvs: Sequence[str]) -> InfoResultList:
+    """Get the statuses of PVs from the archiver.
+
+    Fetches the statuses in chunks to avoid URL length limits.
+
+    Args:
+        archiver_info (ArchiverMgmtInfo): The archiver info to use.
+        pvs (list[str]): The PVs to get status.
+
+    Returns:
+        InfoResultList: The statuses of the PVs.
+    """
+    pv_statuses: InfoResultList = []
+    for i in range(0, len(pvs), MAX_PVS):
+        validate_pvs = pvs[i : i + MAX_PVS]
+        pv_temp_statuses = archiver_info.get_pv_status(",".join(validate_pvs))
+        pv_statuses.extend(pv_temp_statuses)
+    return pv_statuses
+
+
 def get_statuses(archiver_fqdn: str, pvs: Sequence[str]) -> dict[str, list[str]]:
     """Get the statues and print them.
 
@@ -25,7 +48,7 @@ def get_statuses(archiver_fqdn: str, pvs: Sequence[str]) -> dict[str, list[str]]
     """
     # Validate input
     archiver_info = ArchiverMgmtInfo(archiver_fqdn)
-    pv_statuses: InfoResultList = archiver_info.get_pv_status(",".join(pvs))
+    pv_statuses = get_statuses_from_archiver(archiver_info, pvs)
     statuses_to_pvs: dict[str, list[str]] = {}
     for pv_status in pv_statuses:
         if pv_status["status"] not in statuses_to_pvs:
