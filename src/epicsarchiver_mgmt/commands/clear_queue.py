cf6815dc43d46c0484dde449b79682666c5f65ff
Sky Brewer <sky.brewer@ess.eu>
2025-11-10 10:07:45 +0100

    Expand the clear queue functionality
    Covers more filtering of what pvs to clear from the queue

--- src/epicsarchiver_mgmt/commands/clear_queue.py
+++ src/epicsarchiver_mgmt/commands/clear_queue.py
@@ -2,25 +2,102 @@
 
 from __future__ import annotations
 
+import datetime
+import enum
 import logging
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
-from epicsarchiver.mgmt.archiver_mgmt_info import ArchivingStatus, InfoResultList
+from dateutil.parser import parse
 
 from epicsarchiver_mgmt.archiver.mgmt import (
     ArchiverMgmt,
 )
 from epicsarchiver_mgmt.commands import basic_commands
-from epicsarchiver_mgmt.commands.statuses import get_statuses_from_archiver
+from epicsarchiver_mgmt.mgmt_exception import BaseMgmtError
+
+if TYPE_CHECKING:
+    from epicsarchiver.mgmt.archiver_mgmt_info import InfoResultList
 
 LOG: logging.Logger = logging.getLogger(__name__)
 
 
 CURRENT_STATE = "currentState"
-START_STATE = "START"
-SUBMIT_STATE = "ARCHIVE_REQUEST_SUBMITTED"
 
 
-def abort_archiving_pvs_in_queue(pvs: list[str], archiver: ArchiverMgmt, chunking: int = 1000) -> set[str]:
+class ArchivingState(enum.Enum):
+    """Archiving state of a PV in the queue."""
+
+    START = enum.auto()
+    """
+    Start is the start of the archiving workflow.
+    """
+    ARCHIVE_REQUEST_SUBMITTED = enum.auto()
+    """
+    Request has been submitted to the archiver.
+    """
+    METAINFO_GATHERING = enum.auto()
+    """
+    Reading data from the PV to decide archiving policy.
+    """
+    UNKNOWN = enum.auto()
+    """
+    Unknown state.
+    """
+
+    @classmethod
+    def from_str(cls, state: str) -> ArchivingState:
+        """Convert a string to an ArchivingState.
+
+        Args:
+            state (str): The string to convert.
+
+        Returns:
+            ArchivingState: The ArchivingState.
+        """
+        for s in ArchivingState:
+            if s.name == state:
+                return s
+        return ArchivingState.UNKNOWN
+
+
+@dataclass
+class NeverConnectedPV:
+    """Response struct from never connected pvs."""
+
+    request_time: datetime.datetime | None
+    appliance: str
+    pv_name: str
+    current_state: ArchivingState
+    start_of_workflow: datetime.datetime
+
+
+DATE_FORMAT = "%b/%d/%Y %H:%M:%S %Z"
+
+
+def get_never_connected_pvs(archiver: ArchiverMgmt) -> list[NeverConnectedPV]:
+    """Get Never connected pvs from archiver.
+
+    Args:
+        archiver (ArchiverMgmt): The archiver management instance.
+
+    Returns:
+        list[NeverConnectedPV]: List of Never connected pvs.
+    """
+    queue_info: InfoResultList = archiver.never_connected_pvs
+    return [
+        NeverConnectedPV(
+            request_time=parse(item["requestTime"]) if item["requestTime"] and item["requestTime"] != "N/A" else None,
+            appliance=item["appliance"],
+            pv_name=item["pvName"],
+            current_state=ArchivingState.from_str(item[CURRENT_STATE]),
+            start_of_workflow=parse(item["startOfWorkflow"]),
+        )
+        for item in queue_info
+    ]
+
+
+def abort_pvs_in_queue(pvs: list[str], archiver: ArchiverMgmt, chunking: int = 1000) -> set[str]:
     """Get the list of PVs that are currently being archived in the queue and abort them.
 
     Args:
@@ -35,43 +112,87 @@ def abort_archiving_pvs_in_queue(pvs: list[str], archiver: ArchiverMgmt, chunkin
     for i in range(0, len(pvs), chunking):
         chunk = pvs[i : i + chunking]
         LOG.debug("Checking PVs in chunk: %s", chunk)
-        pv_statuses = get_statuses_from_archiver(archiver, chunk)
-
-        if not pv_statuses:
-            LOG.warning("No PV statuses found for chunk: %s", chunk)
-            continue
-        archiving_pvs = {
-            pv_status["pvName"]
-            for pv_status in pv_statuses
-            if ArchivingStatus.from_str(pv_status["status"]) == ArchivingStatus.BeingArchived
-        }
-        if archiving_pvs:
-            basic_commands.AbortCommand().run_command([archiver.hostname], list(archiving_pvs), skip_validation=True)
-            out.update(archiving_pvs)
+        basic_commands.AbortCommand().run_command([archiver.hostname], list(chunk), skip_validation=True)
+        out.update(chunk)
 
     return out
 
 
-def clear_queue(archiver_fqdn: str) -> None:
+class QueueFilter(enum.Enum):
+    """Filter for the queue."""
+
+    ALL = enum.auto()
+    START = enum.auto()
+    METAINFO_GATHERING = enum.auto()
+    ARCHIVING = enum.auto()
+
+
+class OldTimeValueError(BaseMgmtError):
+    """Custom exception for when old_time is required but not provided."""
+
+    def __init__(self, queue_filter: QueueFilter) -> None:
+        """Initialize the OldTimeValueError.
+
+        Args:
+            queue_filter (QueueFilter): The filter that requires old_time.
+        """
+        super().__init__(f"old_time must be provided for the {queue_filter.name} filter")
+
+
+def filter_queue_by_state(
+    archiver: ArchiverMgmt,
+    queue_info: list[NeverConnectedPV],
+    queue_filter: QueueFilter,
+    old_time: datetime.timedelta | None,
+) -> list[str]:
+    """Filter the queue by state.
+
+    Args:
+        archiver (ArchiverMgmt): The archiver management instance.
+        queue_info (list[NeverConnectedPV]): The queue information.
+        queue_filter (QueueFilter): The filter to apply.
+        old_time (datetime.timedelta | None): The time after which a PV is considered stuck.
+
+    Returns:
+        list[NeverConnectedPV]: The filtered queue information.
+
+    Raises:
+        OldTimeValueError: If old_time is required but not provided.
+    """
+    if queue_filter == QueueFilter.ALL:
+        return [item.pv_name for item in queue_info]
+    if queue_filter in {QueueFilter.METAINFO_GATHERING, QueueFilter.START}:
+        if old_time is None:
+            raise OldTimeValueError(queue_filter)
+        return [
+            item.pv_name
+            for item in queue_info
+            if item.start_of_workflow < datetime.datetime.now(tz=datetime.UTC) - old_time
+            and item.current_state == ArchivingState.from_str(queue_filter.name)
+        ]
+    if queue_filter == QueueFilter.ARCHIVING:
+        return archiver.get_archived_pvs([item.pv_name for item in queue_info])
+    return []
+
+
+def clear_queue(archiver_fqdn: str, queue_filter: QueueFilter, old_time: datetime.timedelta | None) -> None:
     """Clear the queue of the archiver.
 
     Args:
         archiver_fqdn (str): The url of the archiver.
+        queue_filter (QueueFilter): The filter to apply.
+        old_time (timedelta | None): The time after which a PV is considered stuck.
     """
-    # Validate input
-
     archiver = ArchiverMgmt(archiver_fqdn)
-    queue_info: InfoResultList = archiver.never_connected_pvs
-
+    queue_info = get_never_connected_pvs(archiver)
     if not queue_info:
         LOG.info("The queue is empty, nothing to clear.")
         return
     # Action
-    pvs = [item["pvName"] for item in queue_info if item[CURRENT_STATE] in {START_STATE, SUBMIT_STATE}]
-    LOG.debug("PVs in the queue: %s", pvs)
-    archiving_pvs = abort_archiving_pvs_in_queue(pvs, archiver)
-    if not archiving_pvs:
-        LOG.info("No PVs were being archived in the queue.")
+    filtered_pvs = filter_queue_by_state(archiver, queue_info, queue_filter, old_time)
+    if not filtered_pvs:
+        LOG.info("No PVs for filter %s found in the queue.", queue_filter.name)
         return
-    LOG.info("PVs being archived in the queue: %s", archiving_pvs)
+    LOG.info("PVs being archived in the queue: %s", filtered_pvs)
+    abort_pvs_in_queue(filtered_pvs, archiver)
     LOG.info("Queue cleared successfully.")
--- tests/commands/test_clear_queue.py
+++ tests/commands/test_clear_queue.py
@@ -1,15 +1,15 @@
+import datetime
 import logging
 from unittest.mock import MagicMock, patch
 
 import pytest
-from epicsarchiver.mgmt.archiver_mgmt_info import ArchivingStatus
 from requests import HTTPError, Response
 
 from epicsarchiver_mgmt.archiver.mgmt import ArchiverMgmt
 from epicsarchiver_mgmt.commands.clear_queue import (
-    CURRENT_STATE,
-    START_STATE,
-    SUBMIT_STATE,
+    ArchivingState,
+    NeverConnectedPV,
+    QueueFilter,
     clear_queue,
 )
 from epicsarchiver_mgmt.commands.validation import RequestHTTPError
@@ -23,53 +23,176 @@ def mock_archiver_mgmt() -> MagicMock:
     return mock_archiver
 
 
-def test_clear_queue_pvs_aborted_successfully(caplog: pytest.LogCaptureFixture, mock_archiver_mgmt: MagicMock) -> None:
-    """Test clear_queue when PVs are found in queue, are being archived, and aborted successfully."""
+NOW = datetime.datetime.now(tz=datetime.UTC)
+START_TIME_DELTA = datetime.timedelta(hours=1)
+METAINFO_TIME_DELTA = datetime.timedelta(minutes=30)
+
+
+@pytest.mark.parametrize(
+    ("queue_filter", "old_time", "queue_info", "get_archived_pvs_return", "mock_now", "expected_aborted_pvs"),
+    [
+        (
+            QueueFilter.ALL,
+            None,
+            [
+                NeverConnectedPV(
+                    request_time=NOW,
+                    appliance="test",
+                    pv_name="PV1",
+                    current_state=ArchivingState.START,
+                    start_of_workflow=NOW,
+                ),
+                NeverConnectedPV(
+                    request_time=NOW,
+                    appliance="test",
+                    pv_name="PV2",
+                    current_state=ArchivingState.ARCHIVE_REQUEST_SUBMITTED,
+                    start_of_workflow=NOW,
+                ),
+            ],
+            None,
+            None,
+            ["PV1", "PV2"],
+        ),
+        (
+            QueueFilter.START,
+            START_TIME_DELTA,
+            [
+                NeverConnectedPV(
+                    request_time=NOW - START_TIME_DELTA * 2,
+                    appliance="test",
+                    pv_name="PV1",
+                    current_state=ArchivingState.START,
+                    start_of_workflow=NOW - START_TIME_DELTA * 2,
+                ),
+                NeverConnectedPV(
+                    request_time=NOW,
+                    appliance="test",
+                    pv_name="PV2",
+                    current_state=ArchivingState.START,
+                    start_of_workflow=NOW,
+                ),
+            ],
+            None,
+            NOW,
+            ["PV1"],
+        ),
+        (
+            QueueFilter.METAINFO_GATHERING,
+            METAINFO_TIME_DELTA,
+            [
+                NeverConnectedPV(
+                    request_time=NOW,
+                    appliance="test",
+                    pv_name="PV1",
+                    current_state=ArchivingState.METAINFO_GATHERING,
+                    start_of_workflow=NOW - METAINFO_TIME_DELTA * 2,
+                ),
+                NeverConnectedPV(
+                    request_time=NOW,
+                    appliance="test",
+                    pv_name="PV2",
+                    current_state=ArchivingState.METAINFO_GATHERING,
+                    start_of_workflow=NOW,
+                ),
+                NeverConnectedPV(
+                    request_time=NOW,
+                    appliance="test",
+                    pv_name="PV3",
+                    current_state=ArchivingState.START,
+                    start_of_workflow=NOW - METAINFO_TIME_DELTA * 2,
+                ),
+            ],
+            None,
+            NOW,
+            ["PV1"],
+        ),
+        (
+            QueueFilter.ARCHIVING,
+            None,
+            [
+                NeverConnectedPV(
+                    request_time=None,
+                    appliance="test",
+                    pv_name="PV1",
+                    current_state=ArchivingState.START,
+                    start_of_workflow=NOW,
+                ),
+                NeverConnectedPV(
+                    request_time=None,
+                    appliance="test",
+                    pv_name="PV2",
+                    current_state=ArchivingState.ARCHIVE_REQUEST_SUBMITTED,
+                    start_of_workflow=NOW,
+                ),
+            ],
+            ["PV1"],
+            None,
+            ["PV1"],
+        ),
+        (
+            QueueFilter.ALL,
+            None,
+            [],
+            None,
+            None,
+            [],
+        ),
+    ],
+)
+def test_clear_queue_successfully(
+    caplog: pytest.LogCaptureFixture,
+    mock_archiver_mgmt: MagicMock,
+    queue_filter: QueueFilter,
+    old_time: datetime.timedelta | None,
+    queue_info: list[NeverConnectedPV],
+    get_archived_pvs_return: list[str] | None,
+    mock_now: datetime.datetime | None,
+    expected_aborted_pvs: list[str],
+) -> None:
+    """Test clear_queue with different filters."""
     caplog.set_level(logging.INFO)
     archiver_fqdn = "archiver.example.com"
 
-    # PVs that will be identified from the queue
-    queue_pvs_to_check = ["PV1", "PV2"]
-    queue_data = [
-        {"pvName": "PV1", CURRENT_STATE: START_STATE},
-        {"pvName": "PV2", CURRENT_STATE: SUBMIT_STATE},
-        {"pvName": "PV3", CURRENT_STATE: "SOME_OTHER_STATE"},  # This PV won't be processed further
-    ]
-
-    # Status for the PVs taken from the queue; PV1 is being archived, PV2 is not.
-    pv_status_data = [
-        {"pvName": "PV1", "status": ArchivingStatus.BeingArchived.value},
-        {"pvName": "PV2", "status": ArchivingStatus.Paused.value},
-    ]
-    # So, only "PV1" should be aborted.
-    expected_aborted_pvs = {"PV1"}
-
-    mock_archiver_mgmt.never_connected_pvs = queue_data
-    mock_archiver_mgmt.get_pv_status.return_value = pv_status_data
+    if get_archived_pvs_return is not None:
+        mock_archiver_mgmt.get_archived_pvs.return_value = get_archived_pvs_return
 
     with (
         patch(
             "epicsarchiver_mgmt.commands.clear_queue.ArchiverMgmt", return_value=mock_archiver_mgmt
         ) as mock_mgmt_class,
-        patch("epicsarchiver_mgmt.commands.clear_queue.basic_commands.AbortCommand.run_command") as mock_abort_command,
+        patch(
+            "epicsarchiver_mgmt.commands.clear_queue.get_never_connected_pvs", return_value=queue_info
+        ) as mock_get_never_connected_pvs,
+        patch("epicsarchiver_mgmt.commands.clear_queue.abort_pvs_in_queue") as mock_abort_pvs_in_queue,
+        patch("epicsarchiver_mgmt.commands.clear_queue.datetime") as mock_datetime,
     ):
-        clear_queue(archiver_fqdn)
+        if mock_now:
+            # We need to mock datetime.datetime within the module, not the global one
+            mock_datetime.datetime.now.return_value = mock_now
+            mock_datetime.UTC = datetime.UTC
+
+        clear_queue(archiver_fqdn, queue_filter, old_time=old_time)
 
         mock_mgmt_class.assert_called_once_with(archiver_fqdn)
-        # The list of PVs passed to get_pv_status depends on the order from the set comprehension
-        # in clear_queue, so we check the content rather than exact string.
-        call_args = mock_archiver_mgmt.get_pv_status.call_args[0][0].split(",")
-        assert sorted(call_args) == sorted(queue_pvs_to_check)
+        mock_get_never_connected_pvs.assert_called_once_with(mock_archiver_mgmt)
 
-        mock_abort_command.assert_called_once()
-        mock_abort_command.assert_called_once_with(
-            [mock_archiver_mgmt.hostname], list(expected_aborted_pvs), skip_validation=True
-        )
+        if not queue_info:
+            assert "The queue is empty, nothing to clear." in caplog.text
+            mock_abort_pvs_in_queue.assert_not_called()
+            return
 
-        # Check logs with caplog.text for INFO level
-        assert f"PVs being archived in the queue: {expected_aborted_pvs}" in caplog.text
-        assert "Queue cleared successfully." in caplog.text
-        assert "No PVs were being archived in the queue." not in caplog.text
+        if expected_aborted_pvs:
+            mock_abort_pvs_in_queue.assert_called_once()
+            call_args = mock_abort_pvs_in_queue.call_args[0][0]
+            assert sorted(call_args) == sorted(expected_aborted_pvs)
+            assert f"PVs being archived in the queue: {expected_aborted_pvs}" in caplog.text
+            assert "Queue cleared successfully." in caplog.text
+            assert "No PVs were being archived in the queue." not in caplog.text
+        else:
+            mock_abort_pvs_in_queue.assert_not_called()
+            assert "No PVs were being archived in the queue." in caplog.text
+            assert "Queue cleared successfully." not in caplog.text
 
 
 def test_clear_queue_http_error_on_abort_command(
@@ -79,12 +202,15 @@ def test_clear_queue_http_error_on_abort_command(
     caplog.set_level(logging.DEBUG)
     archiver_fqdn = "archiver.example.com"
 
-    queue_data = [{"pvName": "PV1", CURRENT_STATE: START_STATE}]
-    pv_status_data = [{"pvName": "PV1", "status": ArchivingStatus.BeingArchived.value}]
-    # PV1 is in queue and being archived, so AbortCommand will be called for it.
-
-    mock_archiver_mgmt.never_connected_pvs = queue_data
-    mock_archiver_mgmt.get_pv_status.return_value = pv_status_data
+    queue_info = [
+        NeverConnectedPV(
+            request_time=NOW,
+            appliance="test",
+            pv_name="PV1",
+            current_state=ArchivingState.START,
+            start_of_workflow=NOW,
+        )
+    ]
 
     http_error_response = Response()
     http_error_response.status_code = 500
@@ -94,14 +220,15 @@ def test_clear_queue_http_error_on_abort_command(
 
     with (
         patch("epicsarchiver_mgmt.commands.clear_queue.ArchiverMgmt", return_value=mock_archiver_mgmt),
-        patch("epicsarchiver_mgmt.commands.clear_queue.basic_commands.AbortCommand.run_command") as mock_abort_command,
+        patch("epicsarchiver_mgmt.commands.clear_queue.get_never_connected_pvs", return_value=queue_info),
+        patch("epicsarchiver_mgmt.commands.clear_queue.abort_pvs_in_queue") as mock_abort_pvs_in_queue,
     ):
-        mock_abort_command.side_effect = abort_command_error
+        mock_abort_pvs_in_queue.side_effect = abort_command_error
 
         with pytest.raises(RequestHTTPError) as exc_info:
-            clear_queue(archiver_fqdn)
+            clear_queue(archiver_fqdn, queue_filter=QueueFilter.ALL, old_time=None)
 
         assert exc_info.value is abort_command_error
-        mock_abort_command.assert_called_once_with([mock_archiver_mgmt.hostname], ["PV1"], skip_validation=True)
+        mock_abort_pvs_in_queue.assert_called_once_with(["PV1"], mock_archiver_mgmt)
         # Check that the final success logs are not present
         assert "Queue cleared successfully." not in caplog.text
